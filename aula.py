1b2ef2ca7a64341a58715deec9f4e2a12281dd2d
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-07-04 20:42:37 -0300

    Create aula.py
    nesse commit está o código da aula 1 do curso de criação de games com python e pygame

---
+++ aula.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.locals import*
+from sys import exit
+
+pygame.init
+
+largura = 680 
+altura = 480 
+
+tela = pygame.display.set_mode((largura,altura))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.QUIT
+            exit()
+    pygame.display.update()
+
