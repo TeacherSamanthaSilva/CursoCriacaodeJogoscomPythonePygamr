75e4d139f93d73db1e6c28914d0437d81394797b
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-07-04 22:33:03 -0300

    Update aula.py
    nessa aula aprendi como fazer uma colisão

--- aula.py
+++ aula.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import*
 from sys import exit
+from random import randint
 
 pygame.init
 
@@ -8,6 +9,8 @@ largura = 680
 altura = 480 
 x = largura / 2
 y = altura / 2
+x_azul = randint(40,600)
+y_azul = randint(50,430)
 relogio = pygame.time.Clock()
 
 tela = pygame.display.set_mode((largura,altura))
@@ -29,7 +32,13 @@ while True:
     if pygame.key.get_pressed()[K_s]:
         y = y + 20
 
-    pygame.draw.rect(tela ,(255,0,0),(x,y,40,50))
+    ret_vermelho = pygame.draw.rect(tela ,(255,0,0),(x,y,40,50))
+    ret_azul = pygame.draw.rect(tela ,(0,0,255),(x_azul,y_azul,40,50))
+
+    if ret_vermelho.colliderect(ret_azul):
+      x_azul = randint(40,600)
+      y_azul = randint(50,430)
+        
     
     pygame.display.update()
 
