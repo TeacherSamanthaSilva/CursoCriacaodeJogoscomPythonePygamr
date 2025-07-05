b641f6267df4295d083dcf8cd1f13f9bbc55b770
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-07-04 21:29:30 -0300

    Update aula.py
    Nessa aula aprendi como  mover os objetos

--- aula.py
+++ aula.py
@@ -6,16 +6,22 @@ pygame.init
 
 largura = 680 
 altura = 480 
+x = largura / 2
+y = 0
+relogio = pygame.time.Clock()
 
 tela = pygame.display.set_mode((largura,altura))
 
 while True:
+    relogio.tick(30)
+    tela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
-            pygame.QUIT
+            pygame.quit()
             exit()
-    pygame.draw.rect(tela ,(255,0,0),(200,300,40,50))
-    pygame.draw.circle(tela,(0,0,255),(300,260),40)
-    pygame.draw.line(tela,(255,255,0),(390,0),(390,600))
+    pygame.draw.rect(tela ,(255,0,0),(x,y,40,50))
+    if y >= altura:
+        y = 0
+    y = y + 1
     pygame.display.update()
 
