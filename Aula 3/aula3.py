491faad74d516b440caa07042e4c0fd41687d181
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-07-23 22:42:56 -0300

    Update aula3.py
    isso foi o que aprendi no curso de python do hugo cursos

--- Aula 3/aula3.py
+++ Aula 3/aula3.py
@@ -8,14 +8,20 @@ def main(): #as definições dos objetos
     sair = False
     relogio = pygame.time.Clock()
     sup = pygame.Surface((200,200))
+    branco = (255,255,255) #as cores em python seguem o padrão RGB
+    azul = (108,194,236)
+    verde = (152,231,114)
+   
 
     while sair != True:
         for event in pygame.event.get(): #captura um evento
             if event.type == pygame.QUIT:
                 sair = True
             relogio.tick(27) #numero de frames por segundo
-            tela.fill((255,255,255))
-            tela.blit(sup) #chamando superficie
+            tela.fill((branco))
+            
+           
+           
 
     pygame.display.update()
     pygame.QUIT
